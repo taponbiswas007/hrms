bbff1d2b8c357258d33091ef0011cb1c7d65ea4d
Tapon chandra sarker <108454446+taponbiswas007@users.noreply.github.com>
2025-03-17 00:16:28 +0600

    home complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,4 +13,36 @@ $(document).ready(function () {
 
 
     });
+
+    // filter items
+    // By default, all content divs are visible
+
+    // Attach change event listener to checkboxes
+    $('input[type="checkbox"]').change(function () {
+        // Check if any checkbox is checked
+        let anyChecked = false;
+        $('input[type="checkbox"]').each(function () {
+            if ($(this).is(':checked')) {
+                anyChecked = true;
+            }
+        });
+
+        // If no checkboxes are checked, show all content divs
+        if (!anyChecked) {
+            $('.myAttendance, .myLeaveBalance, .companyAnnouncement, .holidayList, .workAnniversaries, .birthdays, .employeeOnLeave').show();
+        } else {
+            // Hide all content divs first
+            $('.myAttendance, .myLeaveBalance, .companyAnnouncement, .holidayList, .workAnniversaries, .birthdays, .employeeOnLeave').hide();
+
+            // Show only the checked items
+            $('input[type="checkbox"]:checked').each(function () {
+                $('.' + $(this).attr('id')).show();
+            });
+        }
+    });
 })
+document.querySelectorAll('.dropdown-menu .dropdown-item').forEach(item => {
+    item.addEventListener('click', function (event) {
+        event.stopPropagation(); // Prevent the dropdown from closing
+    });
+});
